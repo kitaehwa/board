6d242b20694516e485fd38b4b8e6a0ae09504912
kitaehwa <kimxx54@gmail.com>
2024-10-16 16:50:52 +0900

    BoardService-글 리스트 동작 listAll() 구현

--- SpringBoard/src/main/java/com/itwillbs/service/BoardService.java
+++ SpringBoard/src/main/java/com/itwillbs/service/BoardService.java
@@ -1,9 +1,14 @@
 package com.itwillbs.service;
 
 import com.itwillbs.domain.BoardVO;
+import java.util.List;
 
 public interface BoardService {
 	
 	// 글쓰기
 	public void regist(BoardVO vo) throws Exception;
+	
+	// 글 리스트 조회
+	public List<BoardVO> listAll() throws Exception;
+	
 }
