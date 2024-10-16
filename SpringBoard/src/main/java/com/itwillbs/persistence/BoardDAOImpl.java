a6fd12c69ba430e36cf8f0d2d681aa1a6883348f
kitaehwa <kimxx54@gmail.com>
2024-10-16 16:25:54 +0900

    mapper 생성, logger 설정, 글쓰기 동작 처리(service,DAO,Mapper) 완료

---
+++ SpringBoard/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
@@ -0,0 +1,34 @@
+package com.itwillbs.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import com.itwillbs.domain.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO{
+	
+	// 디비 연결정보를 처리하는 객체를 주입
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final Logger logger 
+	= LoggerFactory.getLogger(BoardDAOImpl.class);
+	
+	private static final String NameSpace="com.itwillbs.mapper.BoardMapper.";
+	
+	@Override
+	public void createBoard(BoardVO vo) throws Exception {
+		logger.debug("createBoard(BoardVO vo) 호출");
+		logger.debug("디비 연결 - SQL 호출 - 실행");
+		sqlSession.insert(NameSpace+"insertBoard", vo);
+		
+	}
+	
+	
+
+}
